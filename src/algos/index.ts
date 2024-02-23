dfd4e5a38165a54c7642f562e58cf414d16fb813
usounds <46260022+usounds@users.noreply.github.com>
2024-02-23 11:34:04 +0900

    レコード名

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,12 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as ff14user from './ff14user'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [ff14user.shortname]: ff14user.handler,
 }
 
 export default algos
