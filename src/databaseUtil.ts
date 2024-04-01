ee0ceb19414283a16f537a982461388c372fbaae
usounds <grave.evarg@gmail.com>
2024-04-02 00:00:10 +0900

    フィードの削除機能を追加

--- src/databaseUtil.ts
+++ src/databaseUtil.ts
@@ -1,41 +1,41 @@
 import express from 'express'
 import { AppContext } from './config'
 
-const makeRouter =  (ctx: AppContext) => {
+const makeRouter = (ctx: AppContext) => {
     const router = express.Router()
 
     //データ更新
     router.post('/setQuery', async (req: express.Request, res) => {
-        console.log('Operation mode:updateQuery:'+req.body.key)
+        console.log('Operation mode:updateQuery:' + req.body.key)
         const requestWebPasskey = req.headers['x-starrtsky-webpasskey']
 
-        if(process.env.EDIT_WEB_PASSKEY !== undefined && requestWebPasskey !== process.env.EDIT_WEB_PASSKEY){
+        if (process.env.EDIT_WEB_PASSKEY !== undefined && requestWebPasskey !== process.env.EDIT_WEB_PASSKEY) {
             res.sendStatus(401)
-        }else{
+        } else {
 
             //登録時に正規表現をチェック
             try {
-                new RegExp( req.body.inputRegex,'i') 
+                new RegExp(req.body.inputRegex, 'i')
             } catch (err) {
-                console.log('inputRegex error for:'+req.body.key)
-                res.status(500).json({result:'INPUT_REGEX_ERROR',message:'inputRegexの正規表現が正しくありません。inputRegex error. Please input valid regex.'})
+                console.log('inputRegex error for:' + req.body.key)
+                res.status(500).json({ result: 'INPUT_REGEX_ERROR', message: 'inputRegexの正規表現が正しくありません。inputRegex error. Please input valid regex.' })
                 return
             }
 
             try {
-                new RegExp( req.body.invertRegex,'i')
+                new RegExp(req.body.invertRegex, 'i')
             } catch (err) {
-                console.log('invertRegex error for:'+req.body.key)
-                res.status(500).json({result:'INVERT_REGEX_ERROR',message:'invertRegexの正規表現が正しくありません。invertRegex error. Please input valid regex.'})
+                console.log('invertRegex error for:' + req.body.key)
+                res.status(500).json({ result: 'INVERT_REGEX_ERROR', message: 'invertRegexの正規表現が正しくありません。invertRegex error. Please input valid regex.' })
                 return
             }
 
 
             const recordNameRegex = new RegExp(/^[a-z0-9-]{1,15}$/)
-            
-            if(!req.body.recordName.match(recordNameRegex)){
-                console.log('recordNameRegex error for:'+req.body.recordNameRegex)
-                res.status(500).json({result:'RECORDNAME_NOTALPHA',message:'RecordNameは半角英数の15文字以内の小文字です/RecordName shoud be 15 digit lowercase alphabets.'})
+
+            if (!req.body.recordName.match(recordNameRegex)) {
+                console.log('recordNameRegex error for:' + req.body.recordNameRegex)
+                res.status(500).json({ result: 'RECORDNAME_NOTALPHA', message: 'RecordNameは半角英数の15文字以内の小文字です/RecordName shoud be 15 digit lowercase alphabets.' })
                 return
 
             }
@@ -46,16 +46,16 @@ const makeRouter =  (ctx: AppContext) => {
                 return;
             }
 
-            if(isNaN(Number(req.body.initPost))){
-                console.log('initPost error for:'+req.body.initPost)
-                res.status(500).json({result:'NOT_NUMBER_INITPOST',message:'初期取り込み件数は数字のみです。/Initial post count should be number.'})
+            if (isNaN(Number(req.body.initPost))) {
+                console.log('initPost error for:' + req.body.initPost)
+                res.status(500).json({ result: 'NOT_NUMBER_INITPOST', message: '初期取り込み件数は数字のみです。/Initial post count should be number.' })
                 return
 
             }
 
-            if(isNaN(Number(req.body.limitCount))){
-                console.log('limitCount error for:'+req.body.limitCount)
-                res.status(500).json({result:'NOT_NUMBER_LIMITCOUNT',message:'上限件数は数字のみです。/Limit post count should be number.'})
+            if (isNaN(Number(req.body.limitCount))) {
+                console.log('limitCount error for:' + req.body.limitCount)
+                res.status(500).json({ result: 'NOT_NUMBER_LIMITCOUNT', message: '上限件数は数字のみです。/Limit post count should be number.' })
                 return
 
             }
@@ -66,25 +66,25 @@ const makeRouter =  (ctx: AppContext) => {
                 .execute()
 
             let obj = {
-                key:req.body.key,
-                recordName:req.body.recordName,
-                query:       req.body.query,
-                refresh:     req.body.refresh,
-                inputRegex:  req.body.inputRegex,
+                key: req.body.key,
+                recordName: req.body.recordName,
+                query: req.body.query,
+                refresh: req.body.refresh,
+                inputRegex: req.body.inputRegex,
                 invertRegex: req.body.invertRegex,
-                lang:        req.body.lang,
-                initPost:    req.body.initPost,
-                imageOnly:   req.body.imageOnly,
-                pinnedPost:  req.body.pinnedPost,
-                limitCount:  req.body.limitCount,
-                feedName:    req.body.feedName,
-                feedAvatar:  req.body.feedAvatar,
+                lang: req.body.lang,
+                initPost: req.body.initPost,
+                imageOnly: req.body.imageOnly,
+                pinnedPost: req.body.pinnedPost,
+                limitCount: req.body.limitCount,
+                feedName: req.body.feedName,
+                feedAvatar: req.body.feedAvatar,
                 privateFeed: req.body.privateFeed,
-                labelDisable:   req.body.labelDisable,
-                replyDisable:   req.body.replyDisable,
+                labelDisable: req.body.labelDisable,
+                replyDisable: req.body.replyDisable,
                 includeAltText: req.body.includeAltText,
-                feedDescription:req.body.feedDescription,
-                recordCount:0,
+                feedDescription: req.body.feedDescription,
+                recordCount: 0,
             }
 
             ctx.db
@@ -93,7 +93,7 @@ const makeRouter =  (ctx: AppContext) => {
                 .execute()
 
             console.log('Operation mode:updateQuery succeeded.')
-            res.json({result:'OK',message:'更新に成功しました'})
+            res.json({ result: 'OK', message: '更新に成功しました' })
         }
     })
 
@@ -101,55 +101,74 @@ const makeRouter =  (ctx: AppContext) => {
     router.post('/getQuery', async (req: express.Request, res) => {
         console.log('Operation mode:getQuery')
         const requestWebPasskey = req.headers['x-starrtsky-webpasskey']
-        if(process.env.EDIT_WEB_PASSKEY !== undefined && requestWebPasskey !== process.env.EDIT_WEB_PASSKEY){
+        if (process.env.EDIT_WEB_PASSKEY !== undefined && requestWebPasskey !== process.env.EDIT_WEB_PASSKEY) {
             res.sendStatus(401)
-        }else{
+        } else {
             let conditionBuiler = ctx.db
                 .selectFrom('conditions')
                 .selectAll()
                 .where('key', '=', req.body.key)
             const confitionRes = await conditionBuiler.execute()
 
-            if(confitionRes.length===0){
+            if (confitionRes.length === 0) {
                 res.json({
-                    result:'NOT_FOUND',
-                    message:'Specified key not found. '+req.body.key
+                    result: 'NOT_FOUND',
+                    message: 'Specified key not found. ' + req.body.key
                 })
                 return
             }
 
             let returnObj
-            for(let obj of confitionRes){
+            for (let obj of confitionRes) {
                 returnObj = {
-                    result:"OK",
-                    key:obj.key,
-                    recordName:obj.recordName,
-                    query:obj.query,
-                    inputRegex:obj.inputRegex,
-                    invertRegex:obj.invertRegex,
-                    refresh:obj.refresh,
-                    lang:obj.lang,
-                    labelDisable:obj.labelDisable,
-                    replyDisable:obj.replyDisable,
-                    imageOnly:obj.imageOnly,
-                    includeAltText:obj.includeAltText,
-                    initPost:obj.initPost,
-                    pinnedPost:obj.pinnedPost,
-                    lastExecTime:obj.lastExecTime,
-                    feedAvatar:obj.feedAvatar,
-                    feedName:obj.feedName,
-                    feedDescription:obj.feedDescription,
-                    privateFeed:obj.privateFeed,
-                    limitCount:obj.limitCount,
-                    recordCount:obj.recordCount,
-                    queryEngineVersion:'v0.1.2'
+                    result: "OK",
+                    key: obj.key,
+                    recordName: obj.recordName,
+                    query: obj.query,
+                    inputRegex: obj.inputRegex,
+                    invertRegex: obj.invertRegex,
+                    refresh: obj.refresh,
+                    lang: obj.lang,
+                    labelDisable: obj.labelDisable,
+                    replyDisable: obj.replyDisable,
+                    imageOnly: obj.imageOnly,
+                    includeAltText: obj.includeAltText,
+                    initPost: obj.initPost,
+                    pinnedPost: obj.pinnedPost,
+                    lastExecTime: obj.lastExecTime,
+                    feedAvatar: obj.feedAvatar,
+                    feedName: obj.feedName,
+                    feedDescription: obj.feedDescription,
+                    privateFeed: obj.privateFeed,
+                    limitCount: obj.limitCount,
+                    recordCount: obj.recordCount,
+                    queryEngineVersion: 'v0.1.2'
                 }
             }
             res.json(returnObj)
         }
     })
-    
 
-  return router
+    //フィード削除s
+    router.post('/deleteCondition', async (req: express.Request, res) => {
+        console.log('Operation mode:deleteCondition:' + req.body.key)
+        const requestWebPasskey = req.headers['x-starrtsky-webpasskey']
+
+        if (process.env.EDIT_WEB_PASSKEY !== undefined && requestWebPasskey !== process.env.EDIT_WEB_PASSKEY) {
+            res.sendStatus(401)
+        } else {
+            ctx.db
+                .deleteFrom('conditions')
+                .where('key', '=', req.body.key)
+                .execute()
+
+            ctx.db
+                .deleteFrom('post')
+                .where('key', '=', req.body.key)
+                .execute()
+        }
+    })
+
+    return router
 }
 export default makeRouter
