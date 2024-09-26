9814e8c8b26699ed0986ae81ff871443ab2666ca
usounds <grave.evarg@gmail.com>
2024-09-26 19:55:37 +0900

    ライブラリのアップデート

--- update.sh
+++ update.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 cd /opt/StarryskyQueryEngine
 
-echo "-----Step 1:Starryskyの最新の資材を取り込みます-----"
+
+echo "-----Step 1:OSのライブラリをアップデートします-----"
+sudo systemctl stop starrysky.service
+sudo systemctl start starrysky.service
+echo ""
+
+echo "-----Step 2:Starryskyの最新の資材を取り込みます-----"
 git fetch origin
 git reset --hard origin/preview
 git pull --quiet --rebase origin preview
 chmod +x update.sh
 
 echo ""
-echo "-----Step 2:ライブラリをアップデートします-----"
-npm install
+echo "-----Step 3:ライブラリをアップデートします-----"
+sudo apt update
+sudo apt upgrade -y
 
 echo ""
-echo "-----Step 3:サービスを再起動します-----"
+echo "-----Step 4:サービスを再起動します-----"
 sudo systemctl stop starrysky.service
 sudo systemctl start starrysky.service
 
